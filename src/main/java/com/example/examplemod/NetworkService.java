b8e0227f4cfb02269465adb2755c4a21c830b974
mozutangsan <2245141324@qq.com>
2023-01-08 16:10:33 +0800

    first

--- src/main/java/com/example/examplemod/NetworkService.java
+++ src/main/java/com/example/examplemod/NetworkService.java
@@ -70,9 +70,7 @@ public class NetworkService extends Event {
         return networks;
     }
     public void mergeNetwork(Network n1, Network n2) {
-        //n3为合并后的网络
         Network n3 = Network.merge(n1, n2);
-        //将被合并的网络删除
         if (n3 == n1) removeNetwork(n2);
         else removeNetwork(n1);
     }
