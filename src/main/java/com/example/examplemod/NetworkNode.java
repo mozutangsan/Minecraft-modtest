12b65a80734cdc9ae8081e89a8c9d0695e758fa8
mozutangsan <2245141324@qq.com>
2023-01-09 18:13:54 +0800

    first

--- src/main/java/com/example/examplemod/NetworkNode.java
+++ src/main/java/com/example/examplemod/NetworkNode.java
@@ -8,21 +8,16 @@ import java.util.List;
 public class NetworkNode {
     //为每个结点分配一个id
     private int id;
-    //结点所在的网络
     private Network network;
-    //获取结点id
     public int getId() {
         return id;
     }
-    //获取结点所在的网络
     public Network getNetwork() {
         return network;
     }
-    //设置结点所在的网络
     public void setNetwork(Network network) {
         this.network = network;
     }
-    //存储结点所在的BlockEntity
     private BaseBlockEntity blockEntity;
     //构造函数
     public NetworkNode(int id, BaseBlockEntity blockEntity) {
@@ -30,7 +25,6 @@ public class NetworkNode {
         this.blockEntity = blockEntity;
         this.network = null;
     }
-    //获取结点所在位置
     public BlockPos getPos() {
         return blockEntity.getBlockPos();
     }
