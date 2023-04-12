ffef5c4315deecb55415be974d54faae9128d338
mozutangsan <2245141324@qq.com>
2023-04-12 18:32:50 +0800

    first

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,7 +1,11 @@
 package com.example.examplemod;
 
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod("example")
 public class ExampleMod {
+    public ExampleMod(){
+        ItemRegistry.ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
 }
---
+++ src/main/java/com/example/examplemod/ItemRegistry.java
@@ -0,0 +1,12 @@
+package com.example.examplemod;
+
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ItemRegistry {
+    public static final DeferredRegister<Item> ITEMS=DeferredRegister.create(ForgeRegistries.ITEMS,"example");
+    public static final RegistryObject<Item> ironApple=ITEMS.register("iron_apple", IronApple::new);
+
+}
